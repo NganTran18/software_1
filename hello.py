6267da96f0955f45351039838f5acc4db80ba958
NganTran18 <nganlt@metropolia.fi>
2025-08-25 19:39:29 +0300

    initial commit

---
+++ hello.py
@@ -0,0 +1,7 @@
+import math
+print(math.pi)
+a = "="
+print('My name is ngan')
+print(80 * a )
+print('I study at Metropolia')
+
---
+++ second.py
@@ -0,0 +1,25 @@
+#a = int(input("Enter: "))
+#while a > 0:
+ #   print(a*"*")
+  #  a = a - 1
+
+
+#user = int(input('Enter number: '))
+#while user != 0:
+  #  if user % 2 == 0:
+   #     print("even")
+    #else:
+     #   print("odd")
+    #user = int(input('Enter number: '))
+
+user = float(input("Enter number(0-100): "))
+if 90 <= user <= 100:
+    print("A")
+elif 80 <= user <= 89:
+    print("B")
+elif 70 <= user <= 79:
+    print("C")
+else:
+    print("F")
+
+
