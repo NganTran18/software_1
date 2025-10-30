5ca276b274b2c0c09d779af707d660f1469ca032
NganTran18 <nganlt@metropolia.fi>
2025-10-31 00:19:24 +0200

    mod 9, 10

---
+++ mod09/ex01.py
@@ -0,0 +1,42 @@
+class Car():
+    def __init__(self, number, max_speed):
+        self.number = number
+        self.max_speed = max_speed
+        self.current_speed = 0
+        self.distance = 0
+    def info(self):
+        print("Car properties")
+        print(f"Registration number: {self.number}")
+        print(f"Speed:{new_car.max_speed}")
+        print(f"Current speed:{self.current_speed}")
+        print(f"Distance:{new_car.distance}")
+
+    def accelerate(self, change_speed):
+        new_speed = self.current_speed + change_speed
+        if new_speed < 0:
+            self.current_speed = 0
+        elif new_speed > self.max_speed:
+            self.current_speed = self.max_speed
+        else:
+            self.current_speed = new_speed
+
+    def drive(self, hours_number):
+        self.distance += self.current_speed * hours_number
+
+# information
+new_car = Car("ABC-123", 142)
+new_car.info()
+# accelerate
+new_car.accelerate(30)
+print(f"Current speed: {new_car.current_speed}")
+new_car.accelerate(70)
+print(f"New speed: {new_car.current_speed}")
+new_car.accelerate(50)
+print(f"New speed: {new_car.current_speed}")
+new_car.accelerate(-200)
+print(f"Brake & final speed: {new_car.current_speed}")
+# drive
+new_car.drive(1.5)
+print(f"Travelled distance: {new_car.distance}")
+
+
---
+++ mod10/ex.py
@@ -0,0 +1,53 @@
+class Elevator:
+    def __init__(self, bottom, top):
+        self.bottom = bottom
+        self.top = top
+        self.floor = self.bottom
+
+    def floor_up(self):
+        if self.floor < self.top:
+            self.floor += 1
+            print("The elevator is on", self.floor, "floor")
+        else:
+            print("you are in top floor")
+
+    def floor_down(self):
+        if self.floor > self.bottom:
+            self.floor -= 1
+            print("the elevator is on", self.floor, "floor")
+        else:
+            print("you are in bottom floor")
+
+    def go_to_floor(self, main_floor):
+        self.main_floor = main_floor
+        if self.main_floor > self.floor:
+            self.floor_up()
+        elif self.main_floor < self.main_floor:
+            self.floor_down()
+        else:
+            print("invalid")
+
+
+class Building:
+    def __init__(self, bottom, top, num_elevator):
+        self.bottom = bottom
+        self.top = top
+        self.num_elevator = num_elevator
+        self.elevators = [Elevator(self.bottom, self.top) for _ in range(num_elevator)]
+
+    def run_elevator(self, number, destination):
+        self.number = number
+        self.destination = destination
+
+    def fire_alarm(self):
+        print("Fire alam!!!")
+        for i in self.elevators:
+            i.go_to_floor(self.bottom)
+
+
+h = Elevator(1, 10 )
+h.go_to_floor(5)
+h.go_to_floor(1)
+fire = Building(5, 1, 3)
+fire.run_elevator(0, 5)
+fire.fire_alarm()
